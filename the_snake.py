6bfcc7e9311adeea91efa897b5982f1f3ea34c65
Мария Задорина <shmv13m@yandex.ru>
2024-08-17 13:49:16 +0700

    1. Исправлена опечатка в константе SCREEN_CENTER. 2. Добавлены параметры в инициализаторы классов Apple и Snake. 2. Упрощён метод update_direction класса Snake. 3. В класс Snake добавлен метод is_opposit_direction.

--- the_snake.py
+++ the_snake.py
@@ -7,7 +7,7 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
-SCREENS_CENTER = (SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2)
+SCREEN_CENTER = (SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2)
 
 # Направления движения:
 UP = (0, -1)
@@ -46,8 +46,8 @@ class GameObject():
     Описывает их общие атрибуты и методы.
     """
 
-    def __init__(self, position=None, body_color=None):
-        self.position = position if position is not None else SCREENS_CENTER
+    def __init__(self, position=SCREEN_CENTER, body_color=None):
+        self.position = position
         self.body_color = body_color
 
     def draw_one_sector(self, position=None):
@@ -71,9 +71,8 @@ class GameObject():
 class Apple(GameObject):
     """Класс для создания объека 'яблоко'."""
 
-    def __init__(self):
-        super().__init__()
-        self.body_color = APPLE_COLOR
+    def __init__(self, position=SCREEN_CENTER, body_color=APPLE_COLOR):
+        super().__init__(position=position, body_color=body_color)
 
     def randomize_position(self):
         """Метод изменяет позицию яблока, присваивая случайные координаты
@@ -92,20 +91,21 @@ class Apple(GameObject):
 class Snake(GameObject):
     """Класс для создания объекта 'змейка'."""
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, position=SCREEN_CENTER, body_color=SNAKE_COLOR):
+        super().__init__(position=position, body_color=body_color)
         self.reset()
         self.next_direction = None
         self.last = None
-        self.body_color = SNAKE_COLOR
 
-    # Этот метод мне не нужен. Но без него не получается пройти автотесты
-    # и отправить проект на проверку
-    def update_direction(self):
+    def is_opposite_direction(self, direction, current_direction):
+        """Проверяет, не является ли новое направление
+        противоположным текущему.
+        """
+        return direction == tuple(-x for x in current_direction)
+
+    def update_direction(self, new_direction):
         """Обновляет направление после нажатия на кнопку"""
-        if self.next_direction:
-            self.direction = self.next_direction
-            self.next_direction = None
+        self.direction = new_direction
 
     def get_head_position(self):
         """Возвращает координаты головы змейки"""
@@ -115,11 +115,11 @@ class Snake(GameObject):
         """Управляет движением змейки, изменяя список
         с координатами её сегментов.
         """
-        position_now = self.get_head_position()
+        head_x, head_y = self.get_head_position()
         new_x = (
-            position_now[0] + self.direction[0] * GRID_SIZE) % SCREEN_WIDTH
+            head_x + self.direction[0] * GRID_SIZE) % SCREEN_WIDTH
         new_y = (
-            position_now[1] + self.direction[1] * GRID_SIZE) % SCREEN_HEIGHT
+            head_y + self.direction[1] * GRID_SIZE) % SCREEN_HEIGHT
         new_position = (new_x, new_y)
         if len(self.positions) > self.length:
             self.last = self.positions.pop()
@@ -128,7 +128,7 @@ class Snake(GameObject):
     def draw(self):
         """Отрисовывает змейку по заданным координатам."""
         # Отрисовка головы змейки
-        self.draw_one_sector(self.positions[0])
+        self.draw_one_sector(self.get_head_position())
 
         # Затирание последнего сегмента
         if self.last:
@@ -138,7 +138,7 @@ class Snake(GameObject):
     def reset(self):
         """Откатывает состояние змейки к начальному."""
         self.length = 1
-        self.positions = [(SCREENS_CENTER)]
+        self.positions = [(SCREEN_CENTER)]
         self.direction = choice(DIRECTIONS)
 
 
@@ -155,9 +155,12 @@ def handle_keys(game_object):
                 pygame.K_LEFT: LEFT,
                 pygame.K_RIGHT: RIGHT,
             }
-            direction = actions_directions[event.key]
-            if direction != tuple(-x for x in game_object.direction):
-                game_object.direction = direction
+            if event.key in actions_directions:
+                direction = actions_directions[event.key]
+                if not game_object.is_opposite_direction(direction,
+                                                         game_object.direction
+                                                         ):
+                    game_object.update_direction(direction)
 
 
 def main():
@@ -172,11 +175,9 @@ def main():
         snake.move()
         if snake.get_head_position() == apple.position:
             snake.length += 1
-            while True:
+            while apple.position in snake.positions:
                 apple.randomize_position()
-                if apple.position not in snake.positions:
-                    break
-        if snake.get_head_position() in snake.positions[1:]:
+        if snake.get_head_position() in snake.positions[3:]:
             snake.reset()
             screen.fill(BOARD_BACKGROUND_COLOR)
         apple.draw()
